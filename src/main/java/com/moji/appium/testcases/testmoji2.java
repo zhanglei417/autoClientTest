a3c65b8dd0138b6346f17b379622ca6967e1ae44
px <px@px-PC>
2017-06-08 11:05:51 +0800

    update cases

---
+++ src/main/java/com/moji/appium/testcases/testmoji2.java
@@ -0,0 +1,43 @@
+package com.moji.appium.testcases;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.moji.appium.main.AppiumInit;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class testmoji2 extends CaseBaseTest {
+
+@Parameters({"udid","port"})
+@BeforeClass(alwaysRun=true)
+	public void setUp(String udid, String port) throws Exception {
+		driver = new CaseBaseTest().driverInit(udid, port);
+	}
+
+	@Test
+	public void testLogin() throws Exception {
+		driver.swipe(15, 15, 15, 15, 200);
+	}
+	@Test
+	public void testLogout() throws Exception {
+	}
+	@Test
+	public void testSettings() throws Exception {
+		driver.findElementById("Settings");
+	}
+
+	@AfterClass(alwaysRun=true)
+	public void aftertest() throws Exception {
+	    driver.quit();
+	}
+
+
+}
